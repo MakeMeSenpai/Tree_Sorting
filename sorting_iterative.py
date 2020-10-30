eeb36fc6afe513ece83bd9a1fd5638be0f1576bf
Anthony <sean.a.protho1@gmail.com>
2020-10-30 12:19:07 -0700

    taking a break

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -4,7 +4,7 @@ import tracemalloc
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     TODO: Running time: O(n) becuase the time complexity depends on the length of the list
-    TODO: Memory usage: Peak of 9.6e-05MB using short array length of 4 """
+    TODO: Memory usage: Peak of 9.6e-05MB using short array length of 4"""
     # TODO: Check that all adjacent items are in order, return early if sorted
     # grabs an index variable
     for i in range(len(items) - 1):
@@ -21,19 +21,17 @@ def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
     TODO: Running time: O(n)^2 due to nested for loop and dependency of length of list
-    TODO: Memory usage: Peak of 0.000144MB using short array length of 4"""
-    # TODO: sort the list
-    n = len(items) 
-
-    for i in range(n - 1): 
-        # Last i elements are already in place 
-        for j in range(0, n - i - 1): 
-            # Swap if the element found is greater 
-            if items[j] > items[j+1] : 
-                items[j], items[j+1] = items[j+1], items[j]
+    TODO: Memory usage: Peak of 9.6e-05MB using short array length of 4"""
     # TODO: Repeat until all items are in sorted order
-        if is_sorted(items) == True:
-            return items
+    while is_sorted(items) == False:
+        # TODO: sort the list
+        for i in range(len(items) - 1): 
+            # Last i elements are already in place 
+            for j in range(0, len(items) - i - 1): 
+                # Swap if the element found is greater 
+                if items[j] > items[j+1]: 
+                    items[j], items[j+1] = items[j+1], items[j]
+    return items
 
 
 
@@ -42,11 +40,24 @@ def selection_sort(items):
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    
-
     # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    # while is_sorted(items) == False:
+    # checks the first index
+    min = items[0]
+    count = 0
+    for i in range(count, len(items)):
+        # TODO: Find minimum item in unsorted items
+        for j in range(len(items)):
+        # if item's value is lower than min
+            if items[j] < min:
+                # TODO: Swap it with first unsorted item
+                # new minimume is set to item[j]s's vale
+                # item[i + 1] is set to old min
+                min, items[i + 1] = items[j], min
+                # first index is change to minimum value
+                items[0] = min
+                print(items)
+    return items
 
 
 def insertion_sort(items):
@@ -58,11 +69,11 @@ def insertion_sort(items):
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
 
-test = [3, 0, 1, 5]
-print(bubble_sort(test))
+test = [3, 0, 1, -5]
+print(selection_sort(test))
 # this is used to track our memory usage
 tracemalloc.start()
-bubble_sort(test)
+selection_sort(test)
 current, peak = tracemalloc.get_traced_memory()
 print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
 tracemalloc.stop()
